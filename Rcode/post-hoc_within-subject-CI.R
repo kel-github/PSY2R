ded62c47528eddb4b969732b74c79ee485c0fa6c
kelly <getkellygarner@gmail.com>
2023-11-23 10:28:24 +1100

    getting on with functions for simultaneous confidence intervals

--- Rcode/post-hoc_within-subject-CI.R
+++ Rcode/post-hoc_within-subject-CI.R
@@ -9,8 +9,24 @@
 # 2. use equations 7, 8, & 9 to compute the critical values for the CI computation
 
 rm(list=ls())
+library(tidyverse)
 
-data <- read.csv(here("BIRD.csv"))
+###############################################################
+# functions
+###############################################################
+se_cont <- function(MSE, contrast_vector, n){
+  # compute the standard error of the contrast
+  # MSE: mean square error for the effect of interest
+  # contrast_vector: a contrast vector - e.g. one 
+  #  comparing 2 groups would be c(1, -1)
+  sqrt(MSE * sum(contrast_vector^2/n))
+}
+
+
+###############################################################
+# read in data and shape
+###############################################################
+data <- read.csv("BIRD.csv")
 dat <- data
 data <- data %>%
   pivot_longer(
@@ -23,24 +39,40 @@ data$subj <- as.factor(data$subj)
 data$Spacing <- as.factor(data$Spacing)
 data$Spacing <- relevel(data$Spacing, "TWENTY")
 
-# starting with within subject confidence intervals
-## first I compute the standard error for the win contrasts
-
 # first fit the aov, see https://s3-eu-west-1.amazonaws.com/s3-euw1-ap-pe-ws4-cws-documents.ri-prod/9781138024571/chapters/ch11/1_Least-Square_RM_ANOVA_in_R_Using_aov().JLH.pdf
 fit <- aov(Yield~Group*Spacing+Error(subj/Spacing), data=data)
 
+###############################################################
+# between subject confidence intervals
+###############################################################
+
+###############################################################
+# within subject confidence intervals
+###############################################################
+# starting with within subject confidence intervals
+## first I compute the standard error for the win contrasts
+
+
+# the below doesn't give the Kevin Bird, 2002 values
+within_ses <- se.contrast(fit, t(as.matrix(tmp))) 
+
+# can we corroborate this using the formula 3 from Bird 2002?
+summary(fit)
+
+se_cont <- function(MSE, contrast_vector){
+  sqrt(MSE * sum(contrast_vector^2/n))
+}
+
+###############################################################
+# between x within subject confidence intervals
+###############################################################
+
+# the below is useful for knowing how to code contrast vectors
+# but not needed for our purposes as we only need the original
+# contrast vector not replicated over subs
 # see https://rdrr.io/r/stats/se.contrast.html
 a_lev <- gl(3, 16, labels = c("twenty", "forty", "sixty"))
 tw_v_ft <- c(1,-1,0)[a_lev]
 tw_v_sx <- c(1, 0, -1)[a_lev]
 quad <- c(1,-2,1)[a_lev]
 tmp <- rbind(tw_v_ft, tw_v_sx, quad)
-within_ses <- se.contrast(fit, t(as.matrix(tmp))) 
-
-# can we corroborate this using the formula 3 from Bird 2002?
-summary(fit)
-
-MSE = 5.24
-se_cont <- function(MSE, contrast_vector){
-  sqrt(MSE * sum(tw_v_ft^2*16))
-}
