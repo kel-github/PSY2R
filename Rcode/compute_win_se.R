87b15e549bcd6479f15f9b22e73452c52630557a
kelly <getkellygarner@gmail.com>
2023-11-23 17:47:53 +1100

    between and win complete in base r

---
+++ Rcode/compute_win_se.R
@@ -0,0 +1,63 @@
+# code to manually compute contrasts and compute se on the contrast
+# adapted from https://nmmichalak.github.io/nicholas_michalak/blog_entries/2018/nrg07/nrg07.html
+
+library(tidyverse)
+library(knitr)
+library(AMCP)
+library(MASS)
+library(afex)
+
+# select from dplyr
+select <- dplyr::select
+recode <- dplyr::recode
+
+data <- read.csv("BIRD.csv")
+c1 <- c(1, -1, 0)
+c2 <- c(1, 0, -1)
+c3 <- c(1, -2, 1)
+n = 16
+ng = 4
+nw = 3
+
+ldata <- data %>%
+  pivot_longer(
+    cols = 3:5,
+    names_to = "Spacing",
+    values_to = "Yield")%>%
+  dplyr::select(subj,Group,Spacing,Yield)
+ldata$Group <- as.factor(ldata$Group)
+ldata$subj <- as.factor(ldata$subj)
+ldata$Spacing <- as.factor(ldata$Spacing)
+ldata$Spacing <- relevel(ldata$Spacing, "TWENTY")
+ldata$c1 <- rep(c1, times=n)
+ldata$c2 <- rep(c2, times=n)
+ldata$c3 <- rep(c3, times=n)
+ldata$b1 <- rep(c(0.5, 0.5, -0.5, -0.5), each=nw*ng)
+ldata$b2 <- rep(c(1.0, -1.0, 0.0, -0.0), each=nw*ng)
+ldata$b3 <- rep(c(0.0, 0.0, 1.0, -1.0), each=nw*ng)
+
+a = mean(as.matrix(data[,-c(1,2)]) %*% c1)
+b = mean(as.matrix(data[,-c(1,2)]) %*% c2)
+c = mean(as.matrix(data[,-c(1,2)]) %*% c3)
+est <- c(a, b, c)
+est_w <- est
+
+n <- nrow(data)
+
+# sum of squares for the contrast
+assc <- n * a^2 / sum(c1^2)
+bssc <- n * b^2 / sum(c2^2)
+cssc <- n * c^2 / sum(c3^2)
+ssc <- c(assc, bssc, cssc)
+
+# mean squared error is computed for each 
+mse_c1 <- anova(lm(Yield ~ c1 + c1:b1 + c1:b2 + c1:b3 + c1:subj, data = ldata))['c1:subj','Mean Sq']
+mse_c2 <- anova(lm(Yield ~ c2 + c2:b1 + c2:b2 + c2:b3 + c2:subj, data = ldata))['c2:subj','Mean Sq']
+mse_c3 <- anova(lm(Yield ~ c3 + c3:b1 + c3:b2 + c3:b3 + c3:subj, data = ldata))['c3:subj','Mean Sq']
+
+c1se <- sqrt(mse_c1 * sum(c1^2 / n))
+c2se <- sqrt(mse_c2 * sum(c2^2 / n))
+c3se <- sqrt(mse_c3 * sum(c3^2 / n))/2 # to half the contrast values
+se <- c(c1se, c2se, c3se)
+
+save(se, est_w, file = 'win_se.RData')
--- Rcode/post-hoc-CIs.R
+++ Rcode/post-hoc-CIs.R
@@ -1,13 +1,5 @@
-# thoughts and goals:
-# 1. Kevin's confidence intervals use a value
-# which is the square root of the relevant MANOVA T^2 statistic.
-# Gonna read the 2002 EPM paper to really get the method
-# and then re-decide what to do
-
 # to-do
-# 1. implement use of se.contrast to get the standard error on a contrast
-# 2. use equations 7, 8, & 9 to compute the critical values for the CI computation
-
+# 1. implement between/w-in CIs
 rm(list=ls())
 library(tidyverse)
 
@@ -16,12 +8,59 @@ library(tidyverse)
 ###############################################################
 se_cont <- function(MSE, contrast_vector, n){
   # compute the standard error of the contrast
+  # --
+  # Kwargs
   # MSE: mean square error for the effect of interest
   # contrast_vector: a contrast vector - e.g. one 
   #  comparing 2 groups would be c(1, -1)
+  # n [f, 1]: df error
   sqrt(MSE * sum(contrast_vector^2/n))
 }
 
+contrast_ci <- function(cc, se_contrast){
+  # compute the confidence interval using the
+  # critical constant and the standard error of the
+  # contrast
+  # --
+  # Kwargs
+  # cc [int] - appropriate critical constant
+  # se_contrast 
+  
+  cc * se_contrast
+}
+
+cc_between <- function(v_b, v_e, alpha = 0.05){
+  # compute critical constant for between subject contrasts\
+  # formula (7) from Bird, 2002, https://doi.org/10.1177/0013164402062002001
+  # sqrt(v_b * F_alpha;v_b,v_e)
+  # v_b = df between
+  # v_w = df within
+  # v_e = df error
+  # --
+  # Kwargs
+  # v_b: [f, 1] df between - THIS IS THE DF BTWN FOR THE MODEL
+  # v_e: [f, 1] df error
+  # alpha: [f 1] type 1 error rate
+  
+  # first get critical constant F
+  crit_F <- qf(alpha, v_b, v_e, lower.tail = FALSE)
+  sqrt(v_b * crit_F)
+}
+
+cc_within <- function(v_w, v_e, alpha = .05){
+  # compute critical constant for within subject contrasts\
+  # formula (8) from Bird, 2002, https://doi.org/10.1177/0013164402062002001
+  # sqrt((v_w*v_e)/(v_e-v_w+1) * F_alpha;v_w,v_e-v_w+1)
+  # v_w = df within
+  # v_e = df error
+  # --
+  # Kwargs
+  # v_b: [f, 1] df between - THIS IS THE DF BTWN FOR THE CONTRAST
+  # v_e: [f, 1] df error
+  # alpha: [f 1] type 1 error rate
+  crit_F <- qf(alpha, v_w, v_e-v_w+1, lower.tail = FALSE)
+  sqrt((v_w*v_e)/(v_e-v_w+1) * crit_F)
+}
 
 ###############################################################
 # read in data and shape
@@ -39,40 +78,63 @@ data$subj <- as.factor(data$subj)
 data$Spacing <- as.factor(data$Spacing)
 data$Spacing <- relevel(data$Spacing, "TWENTY")
 
+# some info
+n = length(unique(data$Group))
+
 # first fit the aov, see https://s3-eu-west-1.amazonaws.com/s3-euw1-ap-pe-ws4-cws-documents.ri-prod/9781138024571/chapters/ch11/1_Least-Square_RM_ANOVA_in_R_Using_aov().JLH.pdf
 fit <- aov(Yield~Group*Spacing+Error(subj/Spacing), data=data)
 
 ###############################################################
 # between subject confidence intervals
 ###############################################################
+# define contrasts
+# defining contrasts as a list here for lapply use
+# these contrasts are defined for use in manually
+# written functions for the computation of CC and 
+# se etc
+contrasts_b <- list(c1 <- c(0.5, 0.5, -0.5, -0.5),
+                    c2 <- c(1.0, -1.0, 0, 0),
+                    c3 <- c(0, 0, 1, -1))
+
+v_b <- 3 # model has 4 groups
+MSE_b <- sum(fit$subj$residuals^2)/fit$subj$df.residual
+v_e <- fit$subj$df.residual
+#n = length(unique(data$subj))
+
+# NOTE: when n = 12 (residual df, B1 CIs are correct, but not B2 or B3)
+se_b <- lapply(contrasts_b, se_cont, MSE = MSE_b, n=12)
+cc_b <- cc_between(v_b, v_e)
+
+# ci value to be added or subtracted from contrast value
+# get group means
+ms <- data %>% group_by(Group) %>% summarise(mu = mean(Yield))
+est_b <- unlist(do.call(cbind, lapply(contrasts_b, function(x) x %*% ms$mu)))
+ci_b <- unlist(lapply(se_b, contrast_ci, cc=cc_b))
+est_b - ci_b
+est_b + ci_b
 
 ###############################################################
 # within subject confidence intervals
 ###############################################################
 # starting with within subject confidence intervals
 ## first I compute the standard error for the win contrasts
+contrasts_w <- list(c1 = c(1, -1, 0),
+                    c2 = c(1, 0, -1),
+                    c3 = c(1, -2, 1))
+# load standard errors computed from compute_win_se.R
+load("win_se.RData")
+se_w <- se
+rm(se)
+v_w <- length(contrasts_w$c1) - 1
+cc_w <- cc_within(v_w, v_e)
 
+ci_w <- lapply(se_w, contrast_ci, cc=cc_w)
 
-# the below doesn't give the Kevin Bird, 2002 values
-within_ses <- se.contrast(fit, t(as.matrix(tmp))) 
-
-# can we corroborate this using the formula 3 from Bird 2002?
-summary(fit)
-
-se_cont <- function(MSE, contrast_vector){
-  sqrt(MSE * sum(contrast_vector^2/n))
-}
+# make this pretty
+est_w - do.call(cbind, ci_w)
+est_w
+est_w + do.call(cbind, ci_w)
 
 ###############################################################
 # between x within subject confidence intervals
 ###############################################################
-
-# the below is useful for knowing how to code contrast vectors
-# but not needed for our purposes as we only need the original
-# contrast vector not replicated over subs
-# see https://rdrr.io/r/stats/se.contrast.html
-a_lev <- gl(3, 16, labels = c("twenty", "forty", "sixty"))
-tw_v_ft <- c(1,-1,0)[a_lev]
-tw_v_sx <- c(1, 0, -1)[a_lev]
-quad <- c(1,-2,1)[a_lev]
-tmp <- rbind(tw_v_ft, tw_v_sx, quad)
