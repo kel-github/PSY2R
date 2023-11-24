8d1e2ee04b4ac3d5fb98ea32473b662e3dbd433c
katmarchenko <151693068+katmarchenko@users.noreply.github.com>
2023-11-24 12:06:08 +1100

    Update t_bonf_cc.R

--- functions/t_bonf_cc.R
+++ functions/t_bonf_cc.R
@@ -5,7 +5,7 @@
 cc_bonferroni <- function(v_e, ncontr_family, alpha = 0.05){
   # compute critical constant
   # formula on p205 from Bird, 2002, https://doi.org/10.1177/0013164402062002001
-  #CC = t(v_e)_alpha/2*k where k is the number of contrast in a given family 
+  #CC = t(v_e)_alpha/2*k where k is the number of contrasts in a given family 
   #(i.e., n of contrasts between OR within OR int)
   #inputs:
   #v_e = df error
