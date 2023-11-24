fb928b0d173683dbe660409a236e75226f7d661a
kelly <getkellygarner@gmail.com>
2023-11-24 15:08:31 +1100

    nearly have theta using approx method

--- functions/simul_post-hoc.R
+++ functions/simul_post-hoc.R
@@ -33,3 +33,39 @@ cc_within <- function(v_w, v_e, alpha = .05){
   crit_F <- qf(alpha, v_w, v_e-v_w+1, lower.tail = FALSE)
   sqrt((v_w*v_e)/(v_e-v_w+1) * crit_F)
 }
+
+cc_btwn_win <- function(v_b, v_w, v_e, alpha = 1 - 0.05){
+  # compute the critical constrant for between/within using the 
+  # Tracy-Widom method of Johnstone (2009) 149.171.76.156
+  #
+  # Kwargs:
+  # -- v_b [1, f] df between
+  # -- v_w [1, f] df within
+  # -- v_e [1, f] df error
+  # -- alpha [1, f] 1 - type 1 error rate
+  # 
+  # f_alpha = alphath percentile of F1
+  # F1 = f_alpha (F1 is the Tracy- Widom distribution)
+  # theta_alpha = exp^(mu + f_alpha*sigma) / (1 + e^mu + f_alpha*sigma) eq (6)
+  # 
+  # s = min(v_b, v_w) # these are from Kevin Bird, 2002, EPM
+  # m = (abs(v_b - v_w) - 1)/2
+  # n = (v_e - v_w - 1)/2
+  # N = 2*(s + m + n) + 1
+  #
+  # conversion to Johnstone parameters - see (8)
+  # p = s
+  # m = s + 2n + 1
+  # n = s + 2m + 1
+  # 
+  # now get definition of parameters to compute theta_alpha
+  # mu = 2*log(tan(phi + gamma)/2)
+  # sigma^3 = 16/(N^2) * 1/(sin^2 (phi + gamma) sin(phi) sin (gamma))
+  # 
+  # mu = 2 log tan ((phi + gamma)/2)  eq (9)
+  # sigma_3 = 16/(N^2) * 1/(sin^2(phi + gamma) sin(phi) sin(gamma))
+  #
+  # first use rmstat package to get f_alpha
+  f_alpha <- RMTstat::qtw(alpha)
+  
+}
